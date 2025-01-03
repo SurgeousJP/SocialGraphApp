cbd45d0f4f790438a27460df9112622a153519f1
4nh3k <21520579@gm.uit.edu.vn>
2025-01-04 02:01:12 +0700

    Add List import to FollowsController

--- src/main/java/neo4jproject/springframework/controllers/FollowsController.java
+++ src/main/java/neo4jproject/springframework/controllers/FollowsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest/neo4j/follows")
